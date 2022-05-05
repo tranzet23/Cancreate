f02fca6ed19cee97a644b9352032cc4acc9164a3
tranzet23 <vladislavzyuzyukov@gmail.com>
2022-05-05 10:30:30 +0300

    d

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+//*****BURGER********
+// let body = document.querySelector('body')
+// let burgerBtn = document.querySelector('.burger-menu');
+// let menuContent = document.querySelector('.dropdown-menu__mobile');
+//
+//
+// burgerBtn.addEventListener('click', function () {
+//     console.log(123)
+//     if(menuContent.classList.contains('flex')===false) {
+//         menuContent.classList.add('flex');
+//         burgerBtn.classList.add('burger-close');
+//         menuContent.classList.remove('hide');
+//         body.classList.add('scroll-hide');
+//     }
+//     else {
+//         console.log(123)
+//         menuContent.classList.remove('flex');
+//         burgerBtn.classList.remove('burger-close');
+//         menuContent.classList.add('hide');
+//         body.classList.remove('scroll-hide');
+//     }
+// })
