2ef68368540429a90ee29ca67f44af952f8d04f0
RoxShox <19romale77@mail.ru>
2022-05-19 20:22:23 +0300

    vovaVDO

--- src/js/main.js
+++ src/js/main.js
@@ -323,3 +323,16 @@ $('.services-links__link').on('click', function(){
 	$('.services-links__item').removeClass('services-links__item--active');
 	$(this).parent().addClass('services-links__item--active');
 })
+
+
+// Parallax	
+
+ 
+let bg = document.querySelectorAll('.about-canc__photo');
+for (let i = 0; i < bg.length; i++){
+    window.addEventListener('mousemove', function(e) { 
+        let x = e.clientX / window.innerWidth;
+        let y = e.clientY / window.innerHeight;     
+        bg[i].style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+    });    
+}
